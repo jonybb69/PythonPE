3f08aaabf8a9ad703cb7c0aacaeb0863d6be79a9
jonybb69 <155019560+jonybb69@users.noreply.github.com>
2025-01-15 11:47:31 -0600

    ingresa usuario

---
+++ condicion-if.py
@@ -0,0 +1,12 @@
+#En este documento usaremos la condición IF
+#La condición IF la usamos para tomar desiciónes
+
+#declaramos unas variables
+edad = 7
+
+#Realizamos una evaluación de la condición
+if edad < 18:
+    print("Eres menor de edad")
+else:
+    print("Eres mayor de edad")
+
