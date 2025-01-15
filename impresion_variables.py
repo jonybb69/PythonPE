f495159762fbb287fc8e4d48d75da4dcd078b00b
jonybb69 <155019560+jonybb69@users.noreply.github.com>
2025-01-15 11:27:39 -0600

    Operaciones de mate

---
+++ impresion_variables.py
@@ -0,0 +1,15 @@
+#"impresión de variables"
+nombre = "Jonathan"
+print(nombre)
+
+numero_uno = 10
+numero_dos = 5
+
+#"impresión de sumas"
+print(numero_uno+numero_dos)
+
+#"impresión de encocatenaciones"
+nombre_completo = "Jonathan"
+apellidos_completos = "Guerrero altamirano"
+
+print(nombre_completo + " " + apellidos_completos)
---
+++ operaciones.py
@@ -0,0 +1,15 @@
+#Definición de variables
+a = 10
+b = 5
+
+#Operaciones matemáticas
+suma = a + b
+resta = a - b
+multiplicación = a * b
+division = a / b
+
+#Impresión de resultados
+print("La suma de a y b es:", suma)
+print("La resta de a y b es: ", resta)
+print("La La multiplicación de a y b es :", multiplicación)
+print("La división de a y b es: ", division)
